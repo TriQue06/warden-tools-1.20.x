640da5b06708c60da337fa9add61b97dbbf98d3c
Zyablik <elektrosha88@gmail.com>
2024-07-25 11:32:05 +0300

    Now Echo staff can work in creative without consuming echo shards

--- src/main/java/net/trique/wardentools/item/custom/EchoStaff.java
+++ src/main/java/net/trique/wardentools/item/custom/EchoStaff.java
@@ -54,16 +54,19 @@ public class EchoStaff extends Item {
     @Override
     public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
         if (!world.isClient && user instanceof PlayerEntity player) {
-            ItemStack echoShardStack = findEchoShard(player);
+            if(!player.isCreative()){
+                ItemStack echoShardStack = findEchoShard(player);
+                if (!echoShardStack.isEmpty()) {
+                    spawnSonicBoom(world, user);
 
-            if (!echoShardStack.isEmpty()) {
+                    // Echo Shard'ı bir tane azalt
+                    echoShardStack.decrement(1);
+
+                    player.getItemCooldownManager().set(this, 80);
+                    stack.damage(1, user, x -> x.sendToolBreakStatus(Hand.MAIN_HAND));
+                }
+            }else{
                 spawnSonicBoom(world, user);
-
-                // Echo Shard'ı bir tane azalt
-                echoShardStack.decrement(1);
-
-                player.getItemCooldownManager().set(this, 80);
-                stack.damage(1, user, x -> x.sendToolBreakStatus(Hand.MAIN_HAND));
             }
         }
 
