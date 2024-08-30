b0e9acc1d7fce900860d9940ac230499c4d3cd4d
ZyaBlik <58951048+DarkDan25@users.noreply.github.com>
2024-08-30 09:22:05 +0300

    Added Echo Shrieker animation

---
+++ src/main/java/net/trique/wardentools/data/WardenModelPredicateProvider.java
@@ -0,0 +1,28 @@
+package net.trique.wardentools.data;
+
+import net.fabricmc.fabric.api.object.builder.v1.client.model.FabricModelPredicateProviderRegistry;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.trique.wardentools.item.WardenItems;
+
+public class WardenModelPredicateProvider {
+    public static void regModModels(){
+        registerBow(WardenItems.ECHO_SHRIEKER);
+    }
+
+    private static void registerBow(Item bow){
+        FabricModelPredicateProviderRegistry.register(bow, Identifier.of("pull"),
+                (stack, world, entity, seed)->{
+                    if (entity == null){
+                        return 0.0f;
+                    }
+                    if(entity.getActiveItem() != stack){
+                        return 0.0f;
+                    }
+                    return (float)(stack.getMaxUseTime(entity) - entity.getItemUseTimeLeft()) / 20.0f;
+                });
+        FabricModelPredicateProviderRegistry.register(bow, Identifier.of("pulling"),
+                (stack, world,entity,seed) -> entity != null && entity.isUsingItem()
+                        && entity.getActiveItem() == stack ? 1.0f : 0.0f);
+    }
+}
